bd24a19caf37704853e6c5f5770f771b1acfe96d
Paulo César Lins <paulocsar91@gmail.com>
2022-04-25 13:45:50 -0300

    Update
    Pesos por disciplinas

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 function cursonotas(){
 
     let select = document.getElementById("cursoNotas");
-    let valor = select.options[select.selectedIndex].value;
-    console.log(valor);
+    let nota = select.options[select.selectedIndex].value;
+
+    let notaMin = nota.split(";")[0]
+    let notaMax = nota.split(";")[1]
+    let pesoRedacao = nota.split(";")[2]
+    let pesoMatematica = nota.split(";")[3]
+    let pesoLinguagens = nota.split(";")[4]
+    let pesoHumanas = nota.split(";")[5]
+    let pesoNatureza = nota.split(";")[6]
+
+    let msg1 = document.getElementById("notaMin");
+    let msg2 = document.getElementById("notaMax");
+    let msg3 = document.getElementById("pesoRedacao");
+    let msg4 = document.getElementById("pesoMatematica");
+    let msg5 = document.getElementById("pesoLinguagens");
+    let msg6 = document.getElementById("pesoHumanas");
+    let msg7 = document.getElementById("pesoNatureza");
+    
+    msg1.innerHTML = `${notaMin}`
+    msg2.innerHTML = `${notaMax}`
+    msg3.innerHTML = `${pesoRedacao}`
+    msg4.innerHTML = `${pesoMatematica}`
+    msg5.innerHTML = `${pesoLinguagens}`
+    msg6.innerHTML = `${pesoHumanas}`
+    msg7.innerHTML = `${pesoNatureza}`
+
 }
 
 function notahumanas(){
